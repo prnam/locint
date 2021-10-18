77e6f759287ce3541aacaba5299d42d08b6f82d2
Pranam <68323012+prnam@users.noreply.github.com>
2021-10-19 00:03:25 +0530

    improve time

--- task-one/openweatherapi/historical.py
+++ task-one/openweatherapi/historical.py
@@ -1,3 +1,4 @@
+import asyncio
 import httpx
 import datetime
 
@@ -7,25 +8,39 @@ URL = "https://api.openweathermap.org"
 PATH = "/data/2.5/onecall/timemachine"
 
 
-async def get_data(lat: str, lon: str, dates: list):
+async def make_call(client, url):
+    """Makes async calls to url or set of urls and returns the data"""
+    response = await client.post(url)  # noqa
+    return response
+
+
+async def get_three_days_data(lat: str, lon: str, dates: list):
+    """Gets 3 days historical weather data from OpenWeather API"""
     try:
         print("----- OUTPUT -----")
         import config
+        tasks = []
         async with httpx.AsyncClient() as client:
             for dt in dates:
-                query = "?lat={0}&lon={1}&dt={2}&appid={3}".format(lat, lon, dt, config.api_key) # noqa
-                response = await client.request("POST", "{0}{1}{2}".format(URL, PATH, query))  # noqa
+                query = "?lat={0}&lon={1}&dt={2}&appid={3}".format(lat, lon, dt, config.api_key)  # noqa
+                url = "{0}{1}{2}".format(URL, PATH, query)
+                tasks.append(asyncio.ensure_future(make_call(client, url)))
+
+            responses = await asyncio.gather(*tasks)
+
+            for response in responses:
                 status = response.status_code
-                if(status == 200):
+                if status == 200:
                     data = response.json()['hourly'][4]
                     pressure = data['pressure']
-                    human_time = str(datetime.datetime.utcfromtimestamp(data['dt'])) # noqa
-                    print("{0} UTC | Pressure -> {1}".format(human_time, pressure)) # noqa
+                    human_time = str(datetime.datetime.utcfromtimestamp(data['dt']))  # noqa
+                    print("{0} UTC | Pressure -> {1}".format(human_time, pressure))  # noqa
                 else:
                     data = response.json()
                     print("{0}\nMore Info:-".format(WARN))
                     print(" Status Code: {0}".format(data['cod']))
                     print(" {0} {1}".format(MESSAGE, data['message']))
                     exit(1)
+
     except Exception as ex:
         print("{0} {1} {2}".format(WARN, MESSAGE, ex))
